fe9dfd3565de1abf0cef36f335bcc52683db1b8a
shivyelave <syelave20ite@student.mes.ac.in>
2024-05-18 18:00:19 +0530

    navbar issue fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,60 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavbarOpen: false
+    };
+  }
+
+  toggleNavbar = () => {
+    this.setState(prevState => ({
+      isNavbarOpen: !prevState.isNavbarOpen
+    }));
+  }
+
   render() {
+    const { isNavbarOpen } = this.state;
+    const navbarClass = isNavbarOpen ? "collapse navbar-collapse show" : "collapse navbar-collapse";
+
     return (
       <>
         <nav className="navbar navbar-expand-lg bg-body-tertiary fixed-top mb-3" data-bs-theme="dark">
-            <div className="container-fluid">
-                <a className="navbar-brand" href='/'>News Hub</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                    <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className="nav-link active" aria-current="page" to="/business">business</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className="nav-link active" aria-current="page" to="/science">science</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className="nav-link active" aria-current="page" to="/sports">sports</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className="nav-link active" aria-current="page" to="/technology">technology</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className="nav-link active" aria-current="page" to="/entertainment">entertainment</Link>
-                    </li>
-                    <li className="nav-item">
-                    <Link className="nav-link active" aria-current="page" to="/health">health</Link>
-                    </li>
-                </ul>
-                
-                </div>
+          <div className="container-fluid">
+            <a className="navbar-brand" href='/'>News Hub</a>
+            <button className="navbar-toggler" type="button" onClick={this.toggleNavbar} aria-expanded={isNavbarOpen ? "true" : "false"} aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className={navbarClass} id="navbarSupportedContent">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item" onClick={this.toggleNavbar}>
+                  <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                </li>
+                <li className="nav-item" onClick={this.toggleNavbar}>
+                  <Link className="nav-link active" aria-current="page" to="/business">Business</Link>
+                </li>
+                <li className="nav-item" onClick={this.toggleNavbar}>
+                  <Link className="nav-link active" aria-current="page" to="/science">Science</Link>
+                </li>
+                <li className="nav-item" onClick={this.toggleNavbar}>
+                  <Link className="nav-link active" aria-current="page" to="/sports">Sports</Link>
+                </li>
+                <li className="nav-item" onClick={this.toggleNavbar}>
+                  <Link className="nav-link active" aria-current="page" to="/technology">Technology</Link>
+                </li>
+                <li className="nav-item" onClick={this.toggleNavbar}>
+                  <Link className="nav-link active" aria-current="page" to="/entertainment">Entertainment</Link>
+                </li>
+                <li className="nav-item" onClick={this.toggleNavbar}>
+                  <Link className="nav-link active" aria-current="page" to="/health">Health</Link>
+                </li>
+              </ul>
             </div>
-        </nav>    
- 
+          </div>
+        </nav>
       </>
-    )
+    );
   }
 }
